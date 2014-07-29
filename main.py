5ec0754f0a2087a5ab80dcb7a19c9dc6998c0238
Ultar <patrick.hum@hotmail.com>
2014-07-28 23:05:57 -0400

    added a tilemap

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 __author__ = 'Patrick'
 import pygame
+import tmx
 
 class Game(object):
     def main(self, screen):
 
-        clock = pygame.time.Clock()     ## Game clock
+        clock = pygame.time.Clock()     # Game clock
 
+        self.tilemap = tmx.load('data/maps/intro.tmx', screen.get_size())
+
+        ## Main game loop
         while True:
 
-            clock.tick(30)
-
+            clock.tick(30)      # Slows the while loop to 30 FPS
+            ## Get single keypress events (opposed to holding a key down)
             for event in pygame.event.get():
-                ## If the playe tries to quit the game, exit
+                ## If the player tries to quit the game, exit
                 if event.type == pygame.QUIT:
                     return
 
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_q and pygame.key.get_mods() & pygame.KMOD_CTRL:
+                if event.type == pygame.KEYDOWN and \
+                        pygame.key.get_mods() & pygame.KMOD_CTRL and event.key == pygame.K_q:
                     return
 
-            pygame.display.flip() ## Flips the display
+            pygame.display.flip()   # Loads the image from memory
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((640, 360))

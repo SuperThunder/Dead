fffa70e1bddfacbc80f22581dac1d8d867daa723
Ultar <patrick.hum@hotmail.com>
2014-07-30 17:46:50 -0400

    comments

--- main.py
+++ main.py
@@ -10,20 +10,53 @@ class Player(pygame.sprite.Sprite):
 
         self.rect = pygame.rect.Rect(location, self.image.get_size())
 
-    def update(self, dtim, game):
+        ## Variables for gravity
+        self.resting = False    # If the player is on the ground or not
+        self.dy = 0
 
+    def update(self, dtim, game):
+        ## Return the distance between ticks divided by 1000 multiplied by a variable,
+        ## For movement to be standardized
         def dt(val):
             return (dtim * val)/1000
 
-        key = pygame.key.get_pressed()
-
         dx = 200    # Delta X
 
+        old_r = self.rect.copy()     # Get the rectangle before any changes are made (For collision detection)
+
+        key = pygame.key.get_pressed()      # Get keys that are continually being pressed
         if key[pygame.K_LEFT]:
             self.rect.x -= dt(dx)
 
         if key[pygame.K_RIGHT]:
             self.rect.x += dt(dx)
+
+        if self.resting and key[pygame.K_UP]:
+            self.dy = -600
+            self.resting = False
+
+        self.dy = min(400, self.dy + 60)    # Gravity
+        self.rect.y += dt(self.dy)      # Apply the delta-y to the player
+
+        new_r = self.rect   # Get the new rectangle after changes of distance are made (For collision detection)
+
+        ## From map file find tiles /w name 'blockers'
+        ## If 't' in value blockers move block to old_r.bottom
+        ## Same for rest of 'tlrb' (top,left,right,bottom)
+        for cell in game.tilemap.layers['triggers'].collide(new_r, 'blockers'):
+            blockers = cell['blockers']
+            if 'l' in blockers and old_r.right <= cell.left < new_r.right:
+                new_r.right = cell.left
+            if 'r' in blockers and old_r.left >= cell.right > new_r.left:
+                new_r.left = cell.right
+            if 't' in blockers and old_r.bottom <= cell.top < new_r.bottom:
+                self.resting = True
+                new_r.bottom = cell.top
+                self.dy = 0
+            if 'b' in blockers and old_r.top >= cell.bottom > new_r.top:
+                new_r.top = cell.bottom
+                self.dy = 0
+
         game.tilemap.set_focus(self.rect.x, self.rect.y)
 class Game(object):
     def main(self, screen):
@@ -31,8 +64,8 @@ class Game(object):
         clock = pygame.time.Clock()     # Game clock
 
         bg = pygame.image.load('data/images/bg.png')    # Load image file
-        print screen.get_size()
-        self.tilemap = tmx.load('data/maps/intro.tmx', screen.get_size())
+
+        self.tilemap = tmx.load('data/maps/intro.tmx', screen.get_size())   # Load TMX file
 
         ## Load the UPC
         self.sprites = tmx.SpriteLayer()

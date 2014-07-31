40001cea4798c259093be559904c92e3962ac56e
Ultar <patrick.hum@hotmail.com>
2014-07-31 12:37:51 -0400

    Half working shooting system with sound

--- main.py
+++ main.py
@@ -2,6 +2,34 @@ __author__ = 'Patrick'
 import pygame
 import tmx
 
+class Bullet(pygame.sprite.Sprite):
+    image = pygame.image.load('data/images/bullet.png')
+
+    def __init__(self, location, (x, y), *groups):
+
+        super(Bullet, self).__init__(*groups)
+        self.rect = pygame.rect.Rect(location, self.image.get_size())     # Setup rect
+        x, y = float(x), float(y)
+        self.dx = float(x/y)
+        self.dy = -1   # Bullet Direction
+        self.lifespan = 1
+
+
+    def update(self, dtim, game):
+        def dt(val):
+            return (dtim * val)/1000
+
+        self.lifespan -= dt(1)
+        if self.lifespan < 0:
+            self.kill()
+            return
+
+        self.rect.x += self.dx * dt(100)
+        self.rect.y += self.dy * dt(100)
+
+       # if pygame.sprite.spritecollide(self, game.enemies, True):
+       #     self.kill
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, location, *groups):
         super(Player, self).__init__(*groups)
@@ -13,6 +41,10 @@ class Player(pygame.sprite.Sprite):
         ## Variables for gravity
         self.resting = False    # If the player is on the ground or not
         self.dy = 0
+
+        self.direction = 0
+
+        self.gun_cooldown = 0       # Gun cooldown
 
     def update(self, dtim, game):
         ## Return the distance between ticks divided by 1000 multiplied by a variable,
@@ -34,6 +66,12 @@ class Player(pygame.sprite.Sprite):
         if self.resting and key[pygame.K_UP]:
             self.dy = -600
             self.resting = False
+
+        if key[pygame.K_SPACE] and not self.gun_cooldown:
+            game.gun_sound.play()
+            Bullet(self.rect.midleft, pygame.mouse.get_pos(), game.sprites)
+            self.gun_cooldown = 10
+        self.gun_cooldown = max(0, self.gun_cooldown - 1)
 
         self.dy = min(400, self.dy + 60)    # Gravity
         self.rect.y += dt(self.dy)      # Apply the delta-y to the player
@@ -58,6 +96,7 @@ class Player(pygame.sprite.Sprite):
                 self.dy = 0
 
         game.tilemap.set_focus(self.rect.x, self.rect.y)
+
 class Game(object):
     def main(self, screen):
 
@@ -72,6 +111,9 @@ class Game(object):
         start_cell = self.tilemap.layers['triggers'].find('player')[0]
         self.player = Player((start_cell.px, start_cell.py), self.sprites)
         self.tilemap.layers.append(self.sprites)
+
+        ## Sounds
+        self.gun_sound = pygame.mixer.Sound('data/sounds/shot.wav')
 
         ## Main game loop
         while True:
@@ -90,6 +132,8 @@ class Game(object):
             screen.blit(bg, (0, 0))     # Shows the background image
             self.tilemap.update(dt, self)
             self.tilemap.draw(screen)
+
+
             pygame.display.flip()   # Loads the image from memory
 
 if __name__ == '__main__':
